6688bcbef40415e78e6b1e53ca1fb3ca676d8e5f
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2024-08-10 19:44:42 -0700

    update

--- main.cc
+++ main.cc
@@ -1,5 +1,31 @@
+#include <zmqpp/zmqpp.hpp>
+#include <string>
 #include <iostream>
 
-int main(int argc, char** argv) {
-  std::cout << "Hi!" << std::endl;
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  // initialize the 0MQ context
+  zmqpp::context context;
+
+  // create and bind a server socket
+  zmqpp::socket server (context, zmqpp::socket_type::push);
+  server.bind("tcp://*:9000");
+
+  // create and connect a client socket
+  zmqpp::socket client (context, zmqpp::socket_type::pull);
+  client.connect("tcp://127.0.0.1:9000");
+
+  // Send a single message from server to client
+  zmqpp::message request;
+  request << "Hello";
+  server.send(request);
+
+  zmqpp::message response;
+  client.receive(response);
+  
+  assert("Hello" == response.get(0));
+  std::cout << "Grasslands test OK" << std::endl;
+
+  return 0;
 }
