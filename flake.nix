ef532a665b10627d464198d6a27070aae2905028
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2024-08-10 19:00:35 -0700

    add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "ZmqTest";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-darwin.default =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "hello";
+        src = self;
+        buildPhase = "gcc -o hello ./hello.c";
+        installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+      };
+
+  };
+}
